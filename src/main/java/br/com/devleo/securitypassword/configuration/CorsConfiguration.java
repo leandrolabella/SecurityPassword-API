4a9978e3908a6a2b7b24fe61ebacd1e115bb32af
Leandro Silva <leandro.silva.0744@gmail.com>
2023-09-16 19:16:45 -0300

    feat: updated cors configuration

--- src/main/java/br/com/devleo/securitypassword/configuration/CorsConfiguration.java
+++ src/main/java/br/com/devleo/securitypassword/configuration/CorsConfiguration.java
@@ -2,16 +2,19 @@ package br.com.devleo.securitypassword.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/api/v1/password/**")
             .allowedOrigins("http://localhost:3000")
-            .allowedMethods("GET");
+            .allowedMethods("GET")
+            .maxAge(3600);
     }
     
 }
--- src/main/java/br/com/devleo/securitypassword/controllers/PasswordController.java
+++ src/main/java/br/com/devleo/securitypassword/controllers/PasswordController.java
@@ -3,6 +3,7 @@ package br.com.devleo.securitypassword.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ import br.com.devleo.securitypassword.services.PasswordService;
 
 @RestController
 @RequestMapping(path = "api/v1/password")
+@CrossOrigin(origins = "*")
 public class PasswordController {
 
     @Autowired
